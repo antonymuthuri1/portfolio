08b3e525c79320a01f270fdae7c616e53f496f85
antonymuthuri1 <antonymuthurithegreat@gmail.com>
2025-02-10 19:25:09 +0300

    11th  commit

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,32 +1,21 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { getFirestore, collection } from "firebase/firestore";
 
-const CLIENT_apiKey = process.env.NEXT_PUBLIC_FIREBASE_apiKey;
-const CLIENT_authDomain = process.env.NEXT_PUBLIC_FIREBASE_authDomain;
-const CLIENT_databaseURL = process.env.NEXT_PUBLIC_FIREBASE_databaseURL;
-const CLIENT_projectId = process.env.NEXT_PUBLIC_FIREBASE_projectId;
-const CLIENT_storageBucket = process.env.NEXT_PUBLIC_FIREBASE_storageBucket;
-const CLIENT_messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId;
-const CLIENT_appId = process.env.NEXT_PUBLIC_FIREBASE_appId;
-const CLIENT_measurementId = process.env.NEXT_PUBLIC_FIREBASE_measurementId;
-
 const firebaseConfig = {
-  apiKey: CLIENT_apiKey,
-  authDomain: CLIENT_authDomain,
-  databaseURL: CLIENT_databaseURL,
-  projectId: CLIENT_projectId,
-  storageBucket: CLIENT_storageBucket,
-  messagingSenderId: CLIENT_messagingSenderId,
-  appId: CLIENT_appId,
-  measurementId: CLIENT_measurementId
-  
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_measurementId
 };
 
-const app = initializeApp(firebaseConfig);
+// Ensure Firebase is initialized only once
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const messagesCollection = collection(db, "messages");
 
-export { auth, db, messagesCollection ,signInWithEmailAndPassword, signOut };
-
+export { auth, db, messagesCollection, signInWithEmailAndPassword, signOut };
