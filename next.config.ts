d3d6558daf9e311bd90b523c51dd6fc269527838
antonymuthuri1 <antonymuthurithegreat@gmail.com>
2025-02-10 18:50:05 +0300

    9th  commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+  },
 };
 
 export default nextConfig;
